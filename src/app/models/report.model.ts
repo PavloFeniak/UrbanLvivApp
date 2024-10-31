327c191b783fcebd2cbd9034635f1ebe446e910f
PavloFeniak <phenyakpablo@gmail.com>
2024-10-31 19:30:59 +0200

    initial project setup

---
+++ src/app/models/report.model.ts
@@ -0,0 +1,97 @@
+export class Report {
+  constructor(
+    private _id: number,
+    private _title: string,
+    private _description: string,
+    private _imageUrl: string,
+    private _typeOfProblem: number,
+    private _creatorId: number,
+    private _location: string,
+    private _timeOfCreation: string,
+    private _priority: string,
+    private _isDote: boolean
+  ) {}
+
+
+  get id(): number {
+    return this._id;
+  }
+
+  set id(value: number) {
+    this._id = value;
+  }
+
+  get title(): string {
+    return this._title;
+  }
+
+  set title(value: string) {
+    this._title = value;
+  }
+
+  get description(): string {
+    return this._description;
+  }
+
+  set description(value: string) {
+    this._description = value;
+  }
+
+  get imageUrl(): string {
+    return this._imageUrl;
+  }
+
+  set imageUrl(value: string) {
+    this._imageUrl = value;
+  }
+
+  get typeOfProblem(): number {
+    return this._typeOfProblem;
+  }
+
+  set typeOfProblem(value: number) {
+    this._typeOfProblem = value;
+  }
+
+  get creatorId(): number {
+    return this._creatorId;
+  }
+
+  set creatorId(value: number) {
+    this._creatorId = value;
+  }
+
+  get location(): string {
+    return this._location;
+  }
+
+  set location(value: string) {
+    this._location = value;
+  }
+
+  get timeOfCreation(): string {
+    return this._timeOfCreation;
+  }
+
+  set timeOfCreation(value: string) {
+    this._timeOfCreation = value;
+  }
+
+  get priority(): string {
+    return this._priority;
+  }
+
+  set priority(value: string) {
+    this._priority = value;
+  }
+
+  get isDote(): boolean {
+    return this._isDote;
+  }
+
+  set isDote(value: boolean) {
+    this._isDote = value;
+  }
+}
+
+
---
+++ src/app/services/report.service.ts
@@ -0,0 +1,31 @@
+import {Injectable, OnInit} from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpHeaders, HttpParams} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {Router} from "@angular/router";
+import {Report} from "../models/report.model";
+import {catchError, map, Observable, Observer, of} from "rxjs";
+
+
+@Injectable({
+  providedIn: 'root' // Додає сервіс у глобальні провайдери
+})
+export class ReportService implements OnInit {
+  private apiUrl = `http://3.87.172.119/api/Report/GetUserReports/1`;
+
+  constructor(private router: Router, private http: HttpClient) {
+  }
+
+  getReports(title?: string): Observable<Report[]>{
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+    return this.http.get<any>(`${this.apiUrl}`, { headers });
+  }
+
+
+
+  ngOnInit(): void {
+
+  }
+
+}
