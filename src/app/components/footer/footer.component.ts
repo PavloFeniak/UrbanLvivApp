f7e71522a6e1d3877663b2396916d2276a505b91
PavloFeniak <phenyakpablo@gmail.com>
2024-11-04 00:33:36 +0200

    update component styles

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  public homeButtonIsChecked: boolean = true;
+  public userButtonIsChecked: boolean = false;
+  public infoButtonIsChecked: boolean = false;
+
+  checkButton(idButton: number): void{
+    if(idButton == 1) {this.homeButtonIsChecked = true; this.userButtonIsChecked = false; this.infoButtonIsChecked = false;}
+    if(idButton == 2) {this.homeButtonIsChecked = false; this.userButtonIsChecked = true; this.infoButtonIsChecked = false;}
+    if(idButton == 3) {this.homeButtonIsChecked = false; this.userButtonIsChecked = false; this.infoButtonIsChecked = true;}
+
+  }
 
 }
