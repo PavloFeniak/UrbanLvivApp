0c206cae1c97dbe1799ec61134f366cab7692421
PavloFeniak <phenyakpablo@gmail.com>
2024-11-19 14:15:08 +0200

    add admin page and report table components

--- src/app/components/admin-page/admin-page.component.ts
+++ src/app/components/admin-page/admin-page.component.ts
@@ -1,12 +1,76 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ReportAdminModel} from '../../models/report.admin.model';
+import {ReportService} from '../../services/report.service';
+import {ActivatedRoute} from '@angular/router';
+import {AdminService} from '../../services/admin.service';
+import {MyReport} from '../../models/report.model';
+import {NgForOf} from '@angular/common';
+import {ReportSmartComponent} from '../report-smart/report-smart.component';
+import {ReportSummaryComponent} from '../report-summary/report-summary.component';
+
 
 @Component({
   selector: 'app-admin-page',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    ReportSmartComponent,
+    ReportSummaryComponent
+  ],
   templateUrl: './admin-page.component.html',
   styleUrl: './admin-page.component.scss'
 })
-export class AdminPageComponent {
+export class AdminPageComponent implements OnInit{
+  reports: ReportAdminModel[] = [];
+  public newReport: number = 0;
+  public inReview: number = 0;
+  public approved: number = 0;
+  public inProgress: number = 0;
+  public completed: number = 0;
+  public rejected: number = 0;
+  public totalReports: number = 0;
+  constructor(private adminService: AdminService, private route: ActivatedRoute){
 
+  }
+  private loadReports(): void {
+    this.adminService.getAllReports().subscribe(
+      (data) => {
+        console.log("data\n", data)
+        this.reports = data.map(item => {
+          const report = new ReportAdminModel(
+            item.id,
+            item.title,
+            item.typeOfProblem,
+            item.creatorId,
+            item.location,
+            item.timeOfCreation,
+            item.priority,
+            item.processingStatus
+          );
+          return report;
+        });
+        console.log(this.reports)
+        this.totalReports = this.reports.length;
+      },
+      (error) => {
+        console.error('Error loading reports:', error);
+      }
+    );
+  }
+  countAllTypeOfReports(){
+    this.reports.forEach(item =>{
+      switch (item.processingStatus){
+        case 0: this.newReport++; break;
+        case 1: this.inReview++; break;
+        case 2: this.approved++; break;
+        case 3: this.inProgress++; break;
+        case 4: this.completed++; break;
+        case 5: this.rejected++; break;
+      }
+    })
+  }
+  ngOnInit(): void {
+    this.loadReports();
+    this.countAllTypeOfReports();
+  }
 }
--- src/app/components/report-summary/report-summary.component.ts
+++ src/app/components/report-summary/report-summary.component.ts
@@ -1,12 +1,151 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {MyReport} from '../../models/report.model';
+import {ReportAdminModel} from '../../models/report.admin.model';
+import {NgIf, NgStyle} from '@angular/common';
+import {AdminService} from '../../services/admin.service';
+import {IRouter} from 'express';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-report-summary',
   standalone: true,
-  imports: [],
+  imports: [
+    NgStyle,
+    NgIf
+  ],
   templateUrl: './report-summary.component.html',
   styleUrl: './report-summary.component.scss'
 })
-export class ReportSummaryComponent {
+export class ReportSummaryComponent implements OnInit{
+  constructor(private adminService: AdminService, private router: Router) {
+  }
+  @Input() report!: ReportAdminModel;
+
+  isShow: boolean = false;
+  isShowStatusMenu: boolean = false;
+  isShowPriorityMenu: boolean = false;
+  areButtonsEnabled: boolean = false;
+
+  private tagTexts = [
+    "Problem is not selected", "Roads", "Garbage", "Electricity", "Watter Supply",
+    "Sewage", "Parks", "Street Lightning", "Noise", "Traffic", "Environment",
+    "Public Transport", "Infrastructure", "Health", "Animal Control", "Fire Hazard",
+    "Other", "Unknown"
+  ];
+  private statusTexts = [
+    "New",            // The report has just been created and is awaiting further actions
+    "InReview",       // The report is being reviewed by the authorities or AI
+    "Approved",       // The report has been approved for action
+    "InProgress",     // Actions to resolve the issue are in progress
+    "Completed",      // The issue has been resolved
+    "Rejected"
+  ]
+  getStringProblemType(id: number): string{
+    return this.tagTexts[id];
+  }
+  getStringProblemStatus(id: number): string{
+    return this.statusTexts[id];
+  }
+  changePriority(priority: string): void {
+    this.report.priority = priority;  // Оновлюємо пріоритет у моделі звіту
+    this.isShowPriorityMenu = false;  // Закриваємо меню після вибору
+  }
+
+  // Method to get button label based on priority
+  getButtonLabel(priority: string): string {
+    switch (priority.toLowerCase()) {
+      case 'високий': return 'High';
+      case 'середній': return 'Medium';
+      case 'низький': return 'Low';
+      default: return 'Unknown';
+    }
+  }
+
+  // Method to get button background color based on priority
+  getButtonBackgroundColor(priority: string): string {
+    switch (priority.toLowerCase()) {
+      case 'високий': return '#FCEAEE';
+      case 'середній': return '#FEF4E8';
+      case 'низький': return '#E6FAF8';
+      default: return 'gray';
+    }
+  }
+
+  // Method to get button text color based on priority
+  getButtonTextColor(priority: string): string {
+    switch (priority.toLowerCase()) {
+      case 'високий': return '#EB5C77';
+      case 'низький': return '#39D6CC';
+      case 'середній': return '#F6B566';
+      default: return 'black';
+    }
+  }
+
+
+  changeStatus(priority: number): void {
+    this.report.processingStatus = priority;
+    this.isShowStatusMenu = false;
+  }
+  getButtonStatusLabel(priority: number, report1: ReportAdminModel): string {
+    switch (priority) {
+      case 0: return 'New';
+      case 1: return 'In Review';
+      case 2: return 'Approved';
+      case 3: return 'In Progress';
+      case 4: return 'Completed';
+      case 5: return 'Rejected';
+      default: return 'Unknown';
+    }
+  }
+
+  // Method to get button background color based on priority
+  getButtonStatusBackgroundColor(priority: number): string {
+    switch (priority) {
+      case 0: return '#f1fcea';
+      case 1: return '#FEF4E8';
+      case 2: return '#E6FAF8';
+      case 3: return '#f0eafc';
+      case 4: return '#e8fee9';
+      case 5: return '#FCEAEE';
+      default: return 'gray';
+    }
+  }
+
+  // Method to get button text color based on priority
+  getButtonStatusTextColor(priority: number): string {
+    switch (priority) {
+      case 0: return '#90eb5c';
+      case 1: return '#F6B566';
+      case 2: return '#39D6CC';
+      case 3: return '#9c5ceb';
+      case 4: return '#39d653';
+      case 5: return '#EB5C77';
+      default: return 'black';
+    }
+  }
+
+  showMenu(){
+    this.isShow = !this.isShow;
+  }
+  showStatusMenu(){
+    this.isShowStatusMenu = !this.isShowStatusMenu;
+  }
+  showPriorityMenu(){
+    this.isShowPriorityMenu = !this.isShowPriorityMenu;
+  }
+  enableButtons(){
+    this.areButtonsEnabled = true;
+    this.isShow = false;
+  }
+  submitReportChanges(){
+    this.adminService.updateReportByAdmin(this.report.id, this.report.processingStatus, this.report.priority);
+    this.areButtonsEnabled = false;
+    this.isShow = false;
+  }
+  openReportDetails(id: number) {
+    this.router.navigate(['/report-details', id]);
+  }
+  ngOnInit(): void {
+  }
 
 }
